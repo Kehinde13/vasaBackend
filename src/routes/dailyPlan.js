4144a3cddd94d08561b8f439286be990500bdba6
Kehinde Balogun <balogunkehinde3@gmail.com>
2025-08-04 12:20:47 +0100

    update daily plan controller

--- src/routes/dailyPlan.js
+++ src/routes/dailyPlan.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getDailyPlans, upsertDailyBlock, deleteDailyBlock } from '../controllers/dailyPlanController.js';
+import { getDailyPlan, upsertBlock, deleteBlock } from '../controllers/dailyPlanController.js';
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', protect, getDailyPlans);
-router.post('/upsert-block', protect, upsertDailyBlock);
-router.delete('/delete-block', protect, deleteDailyBlock);
+router.get('/', protect, getDailyPlan);
+router.post('/upsert-block', protect, upsertBlock);
+router.delete('/delete-block', protect, deleteBlock);
 
 export default router;
